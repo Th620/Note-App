467c078afcd330683628d36946d68e25dc249e4f
th <t.hadj11010@gmail.com>
2024-06-08 18:48:05 +0100

    adding the fonctionnality of hiding menu when clicking on the page

--- frontend/note-app/src/components/Header.jsx
+++ frontend/note-app/src/components/Header.jsx
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { CgProfile } from "react-icons/cg";
 
-const Header = () => {
-  const [openMenu, setOpenMenu] = useState(false);
-
+const Header = ({ openMenu, setOpenMenu }) => {
   return (
     <header className="fixed top-0 left-0 bg-white w-screen flex justify-between items-center px-6 pr-8 py-4 z-50 shadow-sm">
       <Link to={"/"}>
@@ -13,10 +11,15 @@ const Header = () => {
       <input className="hidden" type="search" name="search" />
       <div
         className="rounded-full flex items-center justify-center text-3xl"
-        onClick={() => setOpenMenu(!openMenu)}
-      ><CgProfile/></div>
+        onClick={() => setOpenMenu((prev) => !prev)}
+      >
+        <CgProfile />
+      </div>
       {openMenu && (
-        <div className="absolute top-[90%] right-8 w-1/3 md:w-1/6 flex flex-col justify-center items-center transition-all duration-300 bg-slate-200 rounded-sm py-3 gap-y-2">
+        <div
+          id="menu"
+          className="absolute top-[90%] right-8 w-1/3 md:w-1/6 flex flex-col justify-center items-center transition-all duration-300 bg-slate-200 rounded-sm py-3 gap-y-2"
+        >
           <h6>User Name</h6>
           <button className="bg-blue-500 px-3 py-1 pb-2 rounded-sm text-white">
             Log out
--- frontend/note-app/src/pages/Home.jsx
+++ frontend/note-app/src/pages/Home.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import Header from '../components/Header'
-import NotesContainer from '../components/NotesContainer'
+import React, { useState } from "react";
+import Header from "../components/Header";
+import NotesContainer from "../components/NotesContainer";
 
 const Home = () => {
+  const [openMenu, setOpenMenu] = useState(false);
   return (
-    <div className='w-full min-h-screen px-6 md:px-20'>
-      <Header/>
-      <NotesContainer/>
+    <div
+      onClick={(e) => {
+        console.log("test");
+        if (openMenu) {
+          setOpenMenu(false);
+        }
+      }}
+      className="w-full min-h-screen px-6 md:px-20"
+    >
+      <Header openMenu={openMenu} setOpenMenu={setOpenMenu} />
+      <NotesContainer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
